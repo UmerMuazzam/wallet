6ad9a32d4d381045a658dc45a6ed40f2743b1bd8
umer muazzam <umer.muazzam@magnusmage.com>
2024-07-03 17:29:35 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "@/components/admin-panel/AuthProvider";
-import App from "./App";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <App>{children}</App>
-        </AuthProvider>
+        {children}
 
         <Toaster position="bottom-center" reverseOrder={false} />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
- 
-export default function Home() {
+ "use client"
+
+import { useState } from "react";
+
+const page = () => {
+  const [createAcc,setCreateAcc] =useState(false)
+  const [restoreAcc, setRestoreAcc] = useState(false);
+
+  const handleAccount=()=>{
+    setCreateAcc(true)
+    setRestoreAcc(false)
+  }
+  const restoreAccount = () => {
+    setRestoreAcc(true);
+    setCreateAcc(false);
+  };
   return (
-    <main >
-      hello
-    </main>
+    <div>
+      <div>
+        <h1>Creata chain wallet</h1>
+      </div>
+
+      <div>
+        <button onClick={handleAccount}>Create wallet</button> <br />
+        <button onClick={restoreAccount}>Restore wallet</button>
+      </div>
+
+      {/* create account  */}
+      {createAcc && <div> 
+        
+        Enter new password <br />
+        <input type="text" placeholder="new password" />
+        </div>}
+      {/* restore account  */}
+      {restoreAcc && <div> restored</div>}
+    </div>
   );
 }
+
+export default page
