85cba5d36f1328a2c4db4aaea702cc82cf377f44
umer muazzam <umer.muazzam@magnusmage.com>
2024-07-03 17:59:05 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ import { useState } from "react";
 const page = () => {
   const [createAcc,setCreateAcc] =useState(false)
   const [restoreAcc, setRestoreAcc] = useState(false);
+  const [password, setPassword] = useState("");
+  const [mnemonics, setMnemonics] = useState("");
+  // console.log(password)
+
+
+   
 
   const handleAccount=()=>{
     setCreateAcc(true)
@@ -15,24 +21,42 @@ const page = () => {
     setCreateAcc(false);
   };
   return (
-    <div>
-      <div>
+    <div className="container">
+      <div className="h-container">
         <h1>Creata chain wallet</h1>
       </div>
 
-      <div>
-        <button onClick={handleAccount}>Create wallet</button> <br />
+      <div className="buttons">
+        <button onClick={handleAccount}>Create wallet</button> 
         <button onClick={restoreAccount}>Restore wallet</button>
       </div>
 
       {/* create account  */}
-      {createAcc && <div> 
-        
-        Enter new password <br />
-        <input type="text" placeholder="new password" />
-        </div>}
+      {createAcc && (
+        <div className="createAcc">
+          <h3>Create a new account</h3>
+          <h4>Enter new password</h4>
+          <br />
+          <input
+            type="text"
+            placeholder="new password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      )}
       {/* restore account  */}
-      {restoreAcc && <div> restored</div>}
+      {restoreAcc && (
+        <div className="createAcc">
+          <h3>Restore wallet</h3>
+          <h4>Enter new password</h4>
+          <br />
+          <input
+            type="text"
+            placeholder="new password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      )}
     </div>
   );
 }
